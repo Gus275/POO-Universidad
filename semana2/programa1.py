e5b2d635b797f008ebc10eac9fdd32ebffc6e055
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-01-23 14:54:19 +0000

    se actualiza la tabla de README.md

---
+++ semana2/programa1.py
@@ -0,0 +1,11 @@
+"""
+    programa1
+    nombre = gustavo salome
+    fecha = 23/01/23
+    descripcion = en este codigo se van a conocer los comentarios, 
+    multilinea y concateneacion
+"""
+variable1= "hola" #  variable para almacenar una cadena de caracteres
+variable2= " mundo python " #  variable para almacenar una cadena de caracteres
+print(variable1 + variable2)
+

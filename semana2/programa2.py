51b9927fb21971e12985d6f1f5c34ca951188941
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-01-24 14:06:07 +0000

    se actualizo la tabla de README.md

---
+++ semana2/programa2.py
@@ -0,0 +1,14 @@
+"""
+    programa1
+    nombre = gustavo salome
+    fecha = 24/01/23
+    descripcion = se aprenndio los errores 
+    comunes que suelen suceder al intridcir 
+    los codigos ya sea por espacios o faltas 
+    de puntos,comas.
+"""
+
+variable1= "hola" 
+variable2=  619 
+# print(variable1 + variable2) # para la funcion de dos variables
+print("{} {}".format(variable1,variable2)) # funcion para poder tener aspectos entre dar variable con la funcion "formal"

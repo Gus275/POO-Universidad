8152484ba8dd97cee1a24a0d90925699f262b705
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-01-30 14:58:11 +0000

    se actualizo la tabla README.md

---
+++ semana3/programa.py
@@ -0,0 +1,19 @@
+numero1 = input ("numero1 ")
+numero2 = 10
+suma = int(numero1) + numero2 
+print(suma)
+
+numero1 = input("numero1")
+numero2 = 15
+resta = int(numero1) - numero2
+print(resta)
+
+numero1 = input("numero 1")
+numero2 = 8
+multiplicacion = int(numero1) * numero2
+print(multiplicacion)
+
+numero1 = input("numero1 ")
+numero2 = 20
+division = int(numero1) / numero2
+print(division)

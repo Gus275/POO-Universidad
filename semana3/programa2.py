b7a0afececfb80f574a5510e2277feede8f929b4
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-02-01 14:55:31 +0000

    se actualizo la tabla de REAME.md

--- semana3/programa2.py
+++ semana3/programa2.py
@@ -1,33 +1,13 @@
 radio = None
 pi = 3.1416
 
-while True:
-  try:
-    radio = float(input("escriba el radio del circulo"))
-    break
-  except:
-    print("debe escribir un numero")
-
-while True:
-  try:
-    pi = float(input("escribir pi."))
-    break
-  except:
-    print("debe escribir un numero.")
-
+radio = float(input("escriba el radio del circulo"))
+pi = float(input("escribir pi."))
 area = pi * (radio **2)
+
 print("el area del circulo es igual: {}".format (area))
 
 valor = 2
-radio = None
-pi = 3.1416
-while True:
-  try:
-    pi = float(input("escribir pi."))
-    break
-  except:
-    print("debe escribir un numero")
-
-while True:
-  try:
-    radio = float()
+radio = float(input("escriba el radio del circulo")
+ = float()
+perimetro = (pi * radio)
---
+++ semana3/programa3.py
@@ -0,0 +1,14 @@
+"""
+    programa3
+    nombre = gustavo salome
+    fecha = 01/02/23
+    descripcion = "comparar 2 numeros enteros 
+    e imprimir el numero mayor"
+"""
+
+numero1 = int(input("numero1"))
+numero2 = int(input("numero2"))
+if numero1 > numero2 :
+  print(numero1)
+else:
+  print(numero2)

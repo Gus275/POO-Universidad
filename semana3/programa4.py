8700135b825f0e5d927d69383b07d461f33f8a8e
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-02-20 20:30:47 +0000

    se actualizo la tabla README.md

--- semana3/programa4.py
+++ semana3/programa4.py
@@ -4,15 +4,15 @@
     fecha = 30/01/23
     descripcion = operaciones basicas
 """
-numero1 = int(input(" Numero 1: "))  #  input devuelve siempre un tipo de dato string, con la funcion casting decidimos que tipo de dato se requiere 
- numero2 = 10  #  Tipo de dato int   
- suma = int(numero1) + numero2  #  Tambien se puede hacer el casting cuando utilicemos el dato  
- resta = numero1-numero2# se hace la operacion de las dos variables que son numero1 y numero2 
- multiplicacion = numero1*numero2# se hace la operacion de las dos variables que son numero1 y numero2 
- division = numero1/numero2#Se hace la operacion de las dos variables que son numero1 y numero2 
- potencia = numero1**numero2 # se hace la operacion de la dos variables que son numero1 y numero2 
- print ("suma =",suma) # se imprime el resultado de la suma 
- print ("resta =",resta) # se imprime el resultado de la resta 
- print ("Multiplicación =",multiplicacion) # se imprime el resultado de la multiplicacion 
- print ("División =",division) # se  imprime el resultado de la division 
- print ("Potencia =",potencia) #se imprime el resultado de la potencia
+numero1 = int(input(" Numero 1:  "))  #  iput devuelve siempre un tipo de dato string, con la funcion casting decidimos que tipo de dato se requiere
+numero2 = 10  #  Tipo de dato int  
+suma = int(numero1) + numero2  #  Tambien se puede hacer el casting cuando utilicemos el dato 
+resta = numero1-numero2# se hace la operacion de las dos variables que son numero1 y numero2
+multiplicacion = numero1*numero2# se hace la operacion de las dos variables que son numero1 y numero2
+division = numero1/numero2#Se hace la operacion de las dos variables que son numero1 y numero2
+potencia = numero1**numero2 # se hace la operacion de la dos variables que son numero1 y numero2
+print ("suma =",suma) # se imprime el resultado de la suma
+print ("resta =",resta) # se imprime el resultado de la resta
+print ("Multiplicación =",multiplicacion) # se imprime el resultado de la multiplicacion
+print ("División =",division) # se  imprime el resultado de la division
+print ("Potencia =",potencia) #se imprime el resultado de la potencia

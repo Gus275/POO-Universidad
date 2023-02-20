6041ce65a402d21d5c39b03de085d306c64a826f
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-02-20 21:44:06 +0000

    se actualizo la tabla de README.md

--- semana5/programa14.py
+++ semana5/programa14.py
@@ -1,6 +1,20 @@
-class Profesor(Persona): # crea la clase Profesor que hereda 
-     def __init__(self)-> None: # contructor de la clase  
-         super().__init__() # llama la constructor de la clase Persona 
-         print("Profesor") # imprime el texto profesor 
-  
- objeto_profesor = Profesor() # crea un objetico de la clase Profesor 
+"""
+    programa14
+    Nombre: Gustavo Iván Salome Jimenez
+    Fecha: 30/01/2023
+    Descripcion: Clase persona
+"""
+
+class Persona:
+    __nombre = None # denota falta de valor
+    __edad = None # denota falta de valor
+    def __init__(self) -> None: # definicion creada porcualquier usuario y denota falta de valor
+        
+        print("Persona")  # mostrar texto en pantalla
+        
+    def setNombre(self,nombre:str) -> None:  # definicion creada porcualquier usuario y denota falta de valor
+ 
+        self.__nombre = nombre # modo de cálculo para hacer referencia al contenido del objeto
+        
+    def getNombre(self) -> str: # definicion creada porcualquier usuario
+        return self.__nombre #  final de la función y continúa la ejecución del programa tras la llamada a la función

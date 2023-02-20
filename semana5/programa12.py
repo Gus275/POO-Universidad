0bc06aedd79c3b54b92d80db3a89d38c06bab23e
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-02-20 21:38:23 +0000

    Se actualizo la tabla README.md

--- semana5/programa12.py
+++ semana5/programa12.py
@@ -1,21 +1,37 @@
-""" 
- nombre: Gustavo Iván Salomé Jiménez  
- fecha:14/02/2023 
- descripcion: se conocera la funcion self. 
- """ 
+"""
+    programa12
+    Nombre: Luz Amairani Martinez Monroy
+    Fecha: 13/02/2023
+    Descripcion: Programa que utiliza clase persona e imprime el nombre, correo y carrera
+"""
 
- class Persona(objeto): # crea la persona 
-   def __init__(self):# se crea el tipo de letra 
-          __nombre=None 
-                  print("Persona")# se imprime la variable persona 
-  
- class Alumno(Persona):# se crea la clase alumno persona 
-   def __init__(self):# se crea el tipo de letra 
-                 super().__init__() 
-                 print("Alumno")# imprime la variable alumno 
-  
- objeto_persona=Persona()# se crea el objeto persona 
- objeto_alumno=Alumno()# se cera el objeto alumno 
-  
- objeto_persona.nombre="Dejah Thoris"# se crea el objeto persona y escrible el nombre de la variable 
- print(objeto_persona.nombre)# se imprime el objeto persona.nombre
+class Alumno:
+    __nombre = None # denota falta de valor
+    __matricula = None # denota falta de valor
+    __carrera = None # denota falta de valor
+    def __init__(self): # definicion creada porcualquier usuario
+        print("Alumno") # mostrar texto en pantalla
+    def setNombre(self,nombre): # definicion creada porcualquier usuario
+        self.__nombre = nombre
+    def getNombre(self): # definicion creada porcualquier usuario
+        return self.__nombre  #  final de la función y continúa la ejecución del programa tras la llamada a la función
+    def setMatricula(self,matricula): # definicion creada porcualquier usuario
+        self.__matricula = matricula
+    def getMatricula(self): # definicion creada porcualquier usuario
+        return self.__matricula #  final de la función y continúa la ejecución del programa tras la llamada a la función
+    def setCarrera(self,carrera): # definicion creada porcualquier usuario
+        self.__carrera = carrera
+    def getCarrera(self): # definicion creada porcualquier usuario
+        return self.__carrera #  final de la función y continúa la ejecución del programa tras la llamada a la función
+
+gustavo = Alumno()
+
+gustavo.setNombre("gustavo") 
+print (gustavo.getNombre())
+
+gustavo.setMatricula("1722110099") 
+print (gustavo.getMatricula())
+
+
+gustavo.setCarrera("Desarrollo de Software") 
+print (gustavo.getCarrera())

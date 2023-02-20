c71b00f22d7b0944b8e4e749d560cba48a91244d
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-02-20 21:42:31 +0000

    Se actualizo la tabla README.md

--- semana5/programa13.py
+++ semana5/programa13.py
@@ -1,2 +1,30 @@
+"""
+    programa13
+    Nombre: Gustavo Iván Salome Jimenez
+    Fecha: 14/02/2023
+    Descripcion: Herencia
+"""
 
- 
+class Persona:
+    def __init__(self): # definicion creada porcualquier usuario
+          print("Persona") # mostrar texto en pantalla
+
+class Alumno(Persona):
+      def __init__(self): # definicion creada porcualquier usuario
+            super().__init__() # permite invocar y conservar un método o atributo de una clase
+            print("Alumno") # mostrar texto en pantalla
+
+objeto_persona = Persona()
+objeto_alumno = Alumno()
+
+objeto_persona.nombre ="Dejah Thoris"
+print(objeto_persona.nombre)
+
+objeto_alumno.nombre ="John Carter"
+print(objeto_persona.nombre) # mostrar texto en pantalla
+
+objeto_alumno.email ="john@utectulancingo.edu.mx"
+print(objeto_alumno.email) # mostrar texto en pantalla
+
+print(dir(objeto_persona)) # mostrar texto en pantalla
+print(dir(objeto_alumno)) # mostrar texto en pantalla

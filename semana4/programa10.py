260f06843a14ee5b5c42c143a447decb21c17f09
Gustavo Salome <122888530+Gus275@users.noreply.github.com>
2023-02-20 13:40:52 +0000

    se actualizo la tabla REAME.md

--- semana4/programa10.py
+++ semana4/programa10.py
@@ -1,5 +1,5 @@
 def  mayor(numero1:int,numero2:int):
-       mayor=None
+       mayor = None
  if numero1 > numero2:
    mayor = (numero1)
  elif numero2>numero1:
---
+++ semana5/programa11.py
@@ -0,0 +1,3 @@
+class Persona:
+  Name= None
+  
